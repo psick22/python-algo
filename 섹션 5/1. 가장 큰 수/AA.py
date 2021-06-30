476e428c2cefc340d3bd1ef2a45732613ff13949
psick22 <kgirok@gmail.com>
2021-07-01 02:01:35 +0900

    greedy 2

---
+++ 섹션 5/1. 가장 큰 수/AA.py
@@ -0,0 +1,21 @@
+import sys
+
+# sys.stdin = open("in1.txt", "r")
+
+# 5276823 3
+
+num, m = map(int, input().split())
+num = list(map(int, str(num)))
+stack = []
+for x in num:
+    while stack and m > 0 and stack[-1] < x:
+        stack.pop()
+        m -= 1
+    stack.append(x)
+
+if m != 0:
+    stack = stack[:-m]
+
+res = ''.join(map(str, stack))
+
+print(res)
