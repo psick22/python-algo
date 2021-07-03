4b9586841e1226d0c011f71b3df221b1c698b7c5
psick22 <kgirok@gmail.com>
2021-07-04 03:07:41 +0900

    queue

--- 섹션 5/2. 쇠막대기/AA2.py
+++ 섹션 5/2. 쇠막대기/AA2.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.stdin = open("in3.txt", "r")
 
 a = input()
--- 섹션 5/7. 교육과정 설계/AA.py
+++ 섹션 5/7. 교육과정 설계/AA.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
 
-sys.stdin=open("input.txt", "r")
+sys.stdin = open("input.txt", "r")
 need = input()
 n = int(input())
 for i in range(n):
