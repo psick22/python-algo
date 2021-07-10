06b8bda1d2c4fd61a2fbbf3aed8afad81a4ebc4d
psick22 <kgirok@gmail.com>
2021-07-10 20:21:38 +0900

    graph

---
+++ 섹션 8/14. 위상 정렬/AA.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+sys.stdin = open("in1.txt", "r")
+
+n, m = map(int, input().split())
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+degree = [0] * (n + 1)
+
+dq = deque()
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    degree[b] += 1
+
+for i in range(1, n + 1):
+    if degree[i] == 0:
+        dq.append(i)
+
+while dq:
+    x = dq.popleft()
+    print(x, end=" ")
+    for i in range(1, n + 1):
+        if graph[x][i] == 1:
+            degree[i] -= 1
+            if degree[i] == 0:
+                dq.append(i)
